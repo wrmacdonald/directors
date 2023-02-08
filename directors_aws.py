2e8bcffe35f487d0ace2f137d298cc6c6540741d
wrmacdonald <wrmacdonald2016@gmail.com>
2023-02-08 15:19:28 -0800

    works on aws lambda

---
+++ directors_aws.py
@@ -0,0 +1,107 @@
+import json
+import requests
+import os
+import smtplib 
+
+# Env vars
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
+# Email
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_ADDRESS_RCVR = os.environ.get('EMAIL_ADDRESS_RCVR')
+# TMDB API - 
+TMDB_BASE_URL = 'https://api.themoviedb.org/3'
+
+def lambda_handler(event, context):
+    # Run Context
+    print(event['scheduled'])
+    if event['scheduled']:
+        run_context = 'Scheduled'
+    else:
+        run_context = 'Testing'
+    
+    # Following Data for testing without DB
+    user = {
+        "pid": [1, 30, 28974, 1032, 71609, 10491, 118415, 10757, 4762, 68813, 5655, 12453, 21684, 137427],
+        "pid_num_mc": [158, 33, 18, 167, 19, 39, 42, 47, 50, 20, 52, 78, 51, 32] 
+    }
+
+    # New data dict
+    dir_updates = {
+        'director': [],
+        'pid': [],
+        'num_mc': [],
+        'title': [],
+        'overview': [],
+        'release_date': [],
+    }
+
+    # Iterate through Following list
+    for i in range(len(user["pid"])):
+        pid = user["pid"][i]                       # a person id the user is following (a director)
+        pid_num_mc = user["pid_num_mc"][i]         # number of movie credits for that pid
+
+        # Get Director's name
+        p_response = requests.get(f"{TMDB_BASE_URL}/person/{pid}?api_key={TMDB_API_KEY}")
+        dir_name = (p_response.json()['name'])
+
+        # Get Director's movie credits
+        pmc_response = requests.get(f"{TMDB_BASE_URL}/person/{pid}/movie_credits?api_key={TMDB_API_KEY}")
+
+        # sort the responses: by increasing release_date, with "" dates @ end
+        pmc_res_sorted = sorted(pmc_response.json()['crew'], key=lambda x: (x['release_date'] == "", x['release_date']))
+
+        if (len(pmc_res_sorted) > pid_num_mc):
+            for i in range(1, len(pmc_res_sorted) - pid_num_mc + 1):
+                dir_updates['director'].append(dir_name)
+                dir_updates['pid'].append(pid)
+                dir_updates['num_mc'].append(len(pmc_res_sorted))
+                dir_updates['title'].append(pmc_res_sorted[-i]['original_title'])
+                dir_updates['overview'].append(pmc_res_sorted[-i]['overview'])
+                dir_updates['release_date'].append(pmc_res_sorted[-i]['release_date'])
+
+
+        # no sort
+        # if (len(pmc_response.json()["crew"]) > pid_num_mc):
+        #     for i in range(1, len(pmc_response.json()["crew"]) - pid_num_mc + 1):
+        #         print(f"checking pid:{pid}, saved # credits:{pid_num_mc}, found credits:{len(pmc_response.json()['crew'])}.")
+        #         print(f"New Entry from pid:{pid} - {pmc_response.json()['crew'][-i]['original_title']}")
+        # print("----")
+
+
+    # Build up Message
+    message = ""
+    for i in range(len(dir_updates['director'])):
+        if (i == 0) or (dir_updates['director'][i-1] != dir_updates['director'][i]):
+            message += ("-----\n")
+            message += (f"{dir_updates['director'][i]} - pid ({dir_updates['pid'][i]}) has the following new projects ({dir_updates['num_mc'][i]} total):\n")
+        message += (f"  {dir_updates['title'][i]} - Releasing: {dir_updates['release_date'][i]}\n")
+        message += (f"    Overview: {dir_updates['overview'][i]}\n")
+        message += ("    -\n")
+
+
+    # Send Email
+    # Encode string that's sent over email is unicode encodable
+    unicode_message = message.encode('ascii', 'ignore').decode('ascii')
+
+    # Send Email if there are updates
+    if message:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:  
+            email_address = EMAIL_ADDRESS
+            email_password = EMAIL_PASSWORD
+            connection.login(email_address, email_password)
+            connection.sendmail(
+                from_addr=email_address, 
+                to_addrs=EMAIL_ADDRESS, 
+                msg=f"subject:Director Updates - {run_context} \n\n {unicode_message}"
+            )
+
+    print(message)    # Testing
+    
+    if message: 
+        return 'New Movies Found'
+    else:
+        return 'No New Movies'
+
+
+
